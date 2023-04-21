276ced1bfbb0775c68dabdc8e6d65b87b1ecf638
gkumarihawk <74431929+gkumarihawk@users.noreply.github.com>
2023-04-21 15:26:56 -0500

    yupdate

--- script/script.js
+++ script/script.js
@@ -1,41 +1,42 @@
 function init(){
-	var canvas = document.getElementById('canvas');
-	var myFav1 = new google.maps.LatLng(41.851611, -87.632264);
-	var myFav2 = new google.maps.LatLng(41.893300, -87.620830);
-	var myFav3 = new google.maps.LatLng(41.9691831, -87.6598913);
-	var maps = {
-		center: myFav2,
+	//alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.881416037821374, -87.62371107312794);
+	var myLocation2 = new google.maps.LatLng(42.36753131192254, -87.93324023323575);
+	var myLocation3 = new google.maps.LatLng(41.77236943257747, -88.15194568534602);
+	var mapOptions = {
+		center: myLocation2,
 		zoom: 18,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
 	};
 
-	var myMap = new google.maps.Map(canvas, maps);
+	var myMap = new google.maps.Map(el, mapOptions);
 
 	var marker = new google.maps.Marker({
-		position: myFav1,
+		position: myLocation,
 		map: myMap,
-		//animation: google.maps.Animation.BOUNCE,
-		icon: 'media/iceCream.png'
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/fountain.png'
 		
 	});
 	var marker2 = new google.maps.Marker({
-		position: myFav2,
+		position: myLocation2,
 		map: myMap,
-		//animation: google.maps.Animation.BOUNCE,
-		icon: 'media/indianGarden.png'
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/ride.png'
 		
 	});
 	var marker3 = new google.maps.Marker({
-		position: myFav3,
+		position: myLocation3,
 		map: myMap,
-		//animation: google.maps.Animation.BOUNCE,
-		icon: 'media/greenMill.png'
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/city.png'
 		
 	});
 
-	var contentString ="<h1>Legend Tasty House</h1><p> My favourite icecream place. You should definitely try their Double Trouble </p>";
+	var contentString ="<h1>Millenium Park</h1><p>A public park located in the Loop community area of Chicago </p>";
 
 	var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -45,7 +46,7 @@ function init(){
     	infowindow.open(myMap, marker);
   	});
 	
-var contentString2 ="<h1>Indian Garden Chicago</h1><p>The best Indian Restaurant in Chicago. You should go here on weekend for lunch. </p>";
+var contentString2 ="<h1>Six Flags</h1><p>Thrilling theme/water park featuring more than 100 rides, shows, and attractions. </p>";
 
 	var infowindow2 = new google.maps.InfoWindow({
       content: contentString2
@@ -54,7 +55,7 @@ var contentString2 ="<h1>Indian Garden Chicago</h1><p>The best Indian Restaurant
 	google.maps.event.addListener(marker2, 'mouseover', function() {
     	infowindow2.open(myMap, marker2);
   	});
-var contentString3 ="<h1>The Green Mill</h1><p>It is one of the oldest Italian pub in Chicago. Their cockatils are super cheap (Drink responsibly). </p>";
+var contentString3 ="<h1>Naperville</h1><p>Just 30 minutes from Chicago, Downtown Naperville is the destination of choice for shopping, dining and enjoying.</p>";
 
 	var infowindow3 = new google.maps.InfoWindow({
       content: contentString3
@@ -63,6 +64,8 @@ var contentString3 ="<h1>The Green Mill</h1><p>It is one of the oldest Italian p
 	google.maps.event.addListener(marker3, 'mouseover', function() {
     	infowindow3.open(myMap, marker3);
   	});
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', init);
